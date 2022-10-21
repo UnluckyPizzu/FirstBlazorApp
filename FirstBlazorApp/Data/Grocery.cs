faebf0e3602966d0a70303f215f10ac3ec4e8aaa
Emilio Pizzuto <emiliopizzuto99@gmail.com>
2022-10-21 20:20:16 +0200

    validation, rollback ect

--- FirstBlazorApp/Data/Grocery.cs
+++ FirstBlazorApp/Data/Grocery.cs
@@ -1,8 +1,12 @@
-﻿namespace FirstBlazorApp.Data
+﻿using System.ComponentModel.DataAnnotations;
+namespace FirstBlazorApp.Data
 {
     public class Grocery
     {
+        [Required]
+        [StringLength(15, ErrorMessage = "Name should be under 15 characters"]
         public string Name { get; set; }
+        [Range(1,10000, ErrorMessage ="Less than 10000")]
         public float Price { get; set; }
     }
 }
