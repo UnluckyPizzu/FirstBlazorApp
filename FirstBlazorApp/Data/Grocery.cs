28bd2bdb48b42e606e0357275991e8faa442c82e
Emilio Pizzuto <emiliopizzuto99@gmail.com>
2022-10-23 18:19:50 +0200

    Fetch data api

--- FirstBlazorApp/Data/Grocery.cs
+++ FirstBlazorApp/Data/Grocery.cs
@@ -4,7 +4,7 @@ namespace FirstBlazorApp.Data
     public class Grocery
     {
         [Required]
-        [StringLength(15, ErrorMessage = "Name should be under 15 characters"]
+        [StringLength(15, ErrorMessage = "Name should be under 15 characters")]
         public string Name { get; set; }
         [Range(1,10000, ErrorMessage ="Less than 10000")]
         public float Price { get; set; }
